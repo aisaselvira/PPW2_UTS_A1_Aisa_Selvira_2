51e7b8cccd034d91592b0c0517b59b627f92b31a
Aisa Selvira <aisaselvira2020@gmail.com>
2023-10-11 09:04:05 +0700

    UTS PPW Soal 2

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('products', ProductController::class);
+Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('products/{product}/edit', [ProductController::class, 'update'])->name('products.update');
+Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
